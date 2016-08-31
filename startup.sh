7b110140039d0b8e398857576c4961b5363f1810
mgaydash <mgaydash@dataforma.com>
2016-08-31 00:22:11 +0000

    Use bundle exec to run rake tasks

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@ git clone https://github.com/dmashuda/ja-next-step.git \
 && mv ./ja-next-step/* . \
 && bundle install --path /box \
 && export SECRET_KEY_BASE=$(rake secret) \
-&& rake db:migrate \
-&& rake db:migrate RAILS_ENV=production \
-&& rake db:seed \
-&& rake db:seed RAILS_ENV=production \
+&& bundle exec rake db:migrate \
+&& bundle exec rake db:migrate RAILS_ENV=production \
+&& bundle exec rake db:seed \
+&& bundle exec rake db:seed RAILS_ENV=production \
 && bundle exec rake assets:precompile \
 && chown -R app:app * \
 && /sbin/my_init
