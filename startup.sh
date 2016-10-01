c5de7f4ad033dab06c3faaecca89b54f935275a6
mgaydash <mgaydash@dataforma.com>
2016-10-01 12:42:24 +0000

    Remove development seed from startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 git clone https://github.com/dmashuda/ja-next-step.git \
 && mv ./ja-next-step/* . \
 && bundle install --path /box \
-&& bundle exec rake db:migrate \
 && bundle exec rake db:migrate RAILS_ENV=production \
-&& bundle exec rake db:seed \
 && bundle exec rake db:seed RAILS_ENV=production \
 && bundle exec rake assets:precompile \
 && chown -R app:app * \
