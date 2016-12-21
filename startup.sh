652fb13795123c8f2f9c935d23a42c8665f3bbc5
mgaydash <mgaydash@dataforma.com>
2016-12-21 08:20:20 -0500

    Remove database seed step from startup routine

--- startup.sh
+++ startup.sh
@@ -2,7 +2,6 @@ git clone https://github.com/dmashuda/ja-next-step.git \
 && mv ./ja-next-step/* . \
 && bundle install --path /box \
 && bundle exec rake db:migrate RAILS_ENV=production \
-&& bundle exec rake db:seed RAILS_ENV=production \
 && bundle exec rake assets:precompile \
 && chown -R app:app * \
 && /sbin/my_init
