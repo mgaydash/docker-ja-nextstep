92b0763e55cdfa47fdafa5be33ef99acfa7988e9
Ubuntu <ubuntu@ip-172-31-28-186.ec2.internal>
2017-02-27 16:42:13 +0000

    Checkout git branch after moving project

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
-git clone https://github.com/dmashuda/ja-next-step.git
+git clone https://github.com/dmashuda/ja-next-step.git \
+&& mv ./ja-next-step/* .
+
 if [ -n "$GIT_BRANCH" ]; then
   git checkout $GIT_BRANCH
 fi
 
-mv ./ja-next-step/* . \
-&& bundle install --path /box \
+bundle install --path /box \
 && bundle exec rake db:migrate RAILS_ENV=production \
 && bundle exec rake assets:precompile \
 && chown -R app:app * \
