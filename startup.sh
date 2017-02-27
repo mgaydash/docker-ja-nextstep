fc654ca2030a69b39e2635ded206a3da1e0bfd0d
Ubuntu <ubuntu@ip-172-31-28-186.ec2.internal>
2017-02-27 16:39:52 +0000

    Fix multiline if in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
-git clone https://github.com/dmashuda/ja-next-step.git \
+git clone https://github.com/dmashuda/ja-next-step.git
 if [ -n "$GIT_BRANCH" ]; then
   git checkout $GIT_BRANCH
 fi
-&& mv ./ja-next-step/* . \
+
+mv ./ja-next-step/* . \
 && bundle install --path /box \
 && bundle exec rake db:migrate RAILS_ENV=production \
 && bundle exec rake assets:precompile \
