075c2a3e9a5d49fa236ec2c2c42d19363a724be6
Ubuntu <ubuntu@ip-172-31-28-186.ec2.internal>
2017-02-27 16:45:36 +0000

    Update startup script again

--- startup.sh
+++ startup.sh
@@ -1,11 +1,15 @@
-git clone https://github.com/dmashuda/ja-next-step.git \
-&& mv ./ja-next-step/* .
+DIR="ja-next-step"
+
+git clone https://github.com/dmashuda/ja-next-step.git
 
 if [ -n "$GIT_BRANCH" ]; then
-  git checkout $GIT_BRANCH
+  cd $DIR \
+  && git checkout $GIT_BRANCH \
+  && cd ..
 fi
 
-bundle install --path /box \
+mv ./$DIR/* . \
+&& bundle install --path /box \
 && bundle exec rake db:migrate RAILS_ENV=production \
 && bundle exec rake assets:precompile \
 && chown -R app:app * \
